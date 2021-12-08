d67569d78a54e74098b1221d754c3822e8246238
Jan Kozohorský <jan.kozohorsky@infis.cz>
2021-12-08 14:44:37 +0100

    Updating...

--- utils/localhost.js
+++ utils/localhost.js
@@ -1,5 +1,7 @@
-var bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 const express = require("express");
+const Config = require("../models/Config");
+const Discord = require("discord.js");
 
 const port = 3321;
 const app = express();
@@ -13,7 +15,38 @@ app.get("/", function (req, res, next) {
 });
 
 app.post("/toAdmin", function (req, res, next) {
-  console.log(req.body);
+  var args = req.body.args;
+  const event = req.body.event;
+  Config.findOne(
+    {
+      number: 1,
+    },
+    (err, Gres) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      const user = bot.users.cache.find(
+        (user) => user.id == Gres.botAdminDiscordID[0]
+      );
+
+      user.send(
+        new Discord.MessageEmbed()
+          .setTitle(args[0])
+          .setDescription(
+            (event == "PrintStarted"
+              ? "Started: "
+              : event == "PrintFailed"
+              ? "Failed: "
+              : "Done: ") +
+              args[1] +
+              (event == "PrintFailed" ? " \nReason: " + args[2] : "")
+          )
+          .setTimestamp()
+      );
+    }
+  );
+  res.sendStatus(200);
 });
 
 app.listen(port);
